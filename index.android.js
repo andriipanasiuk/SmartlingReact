358350d08d9815aadff36850dea6c4baf0497914
Andrii Panasiuk <andriipanasiuk@gmail.com>
2017-02-22 14:49:38 +0200

    Create global strings dictionary; fetch strings from native Java module using SmartlingBridge

--- index.android.js
+++ index.android.js
@@ -9,22 +9,52 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  NativeModules,
+  View,
+  DeviceEventEmitter
 } from 'react-native';
+import LocalizedStrings from 'react-native-localization';
+
+
+global.strings = new LocalizedStrings ({
+    "en-US":{
+        welcome:"Welcome to React Native!",
+        get_started:"To get started, edit index.android.js",
+        instructions:"Double tap R on your keyboard to reload,\n"+
+                                "Shake or press menu button for dev menu"
+    }
+});
+
+// Get strings at launch
+NativeModules.SmartlingBridge.getStrings((locale, strings) => {
+    updateStrings(locale, strings);
+});
+
+DeviceEventEmitter.addListener('SmartlingStringsUpdated', (map) => {
+        updateStrings(map.locale, map.strings);
+});
+
+function updateStrings(locale, strings) {
+    global.strings = new LocalizedStrings(strings);
+    global.strings.setLanguage(locale);
+    //TODO update current scene
+    console.log(locale);
+    console.log(global.strings);
+}
 
 class PropertyFinder extends Component {
   render() {
+  console.log('Render');
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          {strings.welcome}
         </Text>
         <Text style={styles.instructions}>
-          To get started, edit index.android.js
+          {strings.get_started}
         </Text>
         <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
+          {strings.instructions}
         </Text>
       </View>
     );
